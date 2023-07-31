bd1e655ba718cba5d51b2a4b4c5e04344f29daff
salehhd123 <71578301+salehhd123@users.noreply.github.com>
2023-07-31 14:40:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,21 @@ Account a1 = new Account("1","saleh",2000);
 Account a2 = new Account("1","saleh",100);
 
 
-//        System.out.println(a1.getBalance());
+        System.out.println(a1.getBalance());
         System.out.println(a1.transferToAccount(a2,3000));
         System.out.println(a1.getBalance());
         System.out.println(a2.getBalance());
-//        a1.credit(1000);
-//        System.out.println(a1.getBalance());
-//
-//
-//        System.out.println("----------------------------");
-//
-//        Employee e1 = new Employee("111","saleh", 100);
-//        System.out.println(e1.getSalary());
-//        System.out.println(e1.getAnnualSalary());
-//        e1.raisedSalary(5);
-//        System.out.println(e1.getAnnualSalary());
-//        System.out.println(e1.toSting());
+        a1.credit(1000);
+       System.out.println(a1.getBalance());
+
+
+       System.out.println("----------------------------");
+
+       Employee e1 = new Employee("111","saleh", 100);
+       System.out.println(e1.getSalary());
+       System.out.println(e1.getAnnualSalary());
+       e1.raisedSalary(5);
+       System.out.println(e1.getAnnualSalary());
+       System.out.println(e1.toSting());
     }
 }
